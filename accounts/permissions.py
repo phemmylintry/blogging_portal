963519cdbac31d7bab8e76e205ee3588c7859ece
Oluwafemi <phemmylintry@gmail.com>
2021-02-04 19:45:12 +0100

    fix

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -3,7 +3,6 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        print("here")
         if request.method in permissions.SAFE_METHODS:
             return True
 
